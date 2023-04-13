3b2c66f07cc726fa1fd527089fdd06a4d4b94a68
Olaoye Ademola <demmydd44@gmail.com>
2023-04-13 17:39:37 +0000

    class basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Defines base geometry."""
+
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
