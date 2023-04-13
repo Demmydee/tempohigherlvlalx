a79c40b80457ef9639f2887393a576eb6b9f8803
Olaoye Ademola <demmydd44@gmail.com>
2023-04-13 00:10:55 +0000

    return an object represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Returns ana object represented by a JSON string."""
+import json
+
+
+def from_json_string(my_str):
+    """Returns a python data structure object represented \n
+	by a JSON string."""
+    return json.loads(my_str)
