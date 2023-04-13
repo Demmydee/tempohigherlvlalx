91f21d2b6d42c18758627503824647b057521c0a
Olaoye Ademola <demmydd44@gmail.com>
2023-04-13 00:23:03 +0000

    write an object to a text file using JSON representation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file using a JSON representation."""
-    with open(filename, mode="m") as a_file:
+    with open(filename, mode="w") as a_file:
         json.dump(my_obj, a_file)
